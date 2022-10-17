48daa48af992c1fc700b196f6d96631dc9803e2e
milkami <milka.milicevic3@gmail.com>
2022-10-17 14:41:00 +0200

    test

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hrz_stage',
+        'USER': 'hrz_app',
+        'PASSWORD': 'xa4ieshauDoh',
+        'HOST': '10.0.41.250',
+        'PORT': '5438',
     }
 }
 
